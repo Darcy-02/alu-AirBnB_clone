c74e18bd1f4b61591291cbf7efbae357c595304a
Darcy-02 <d.mbanza@alustudent.com>
2025-09-13 04:16:42 -0700

    Unittests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unit tests for BaseModel class"""
+
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Tests for BaseModel class"""
+
+    def test_instance_creation(self):
+        """Test that a BaseModel instance is created properly"""
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
+
+    def test_attributes(self):
+        """Test that the instance has correct attributes"""
+        obj = BaseModel()
+        self.assertTrue(hasattr(obj, "id"))
+        self.assertTrue(hasattr(obj, "created_at"))
+        self.assertTrue(hasattr(obj, "updated_at"))
+
+if __name__ == "__main__":
+    unittest.main()
